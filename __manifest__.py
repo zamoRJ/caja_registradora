cd5694c5f920b71980669d1a7314ae2794a1ed96
zamoRJ <55899348+zamoRJ@users.noreply.github.com>
2023-03-29 23:57:51 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,36 +1,20 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Cash register",
-
     'summary': """
         Registration of income / expenses in the cash register.""",
-
     'description': """
         Shows the movements made by the contacts, whether they are in/out, and the date on which they were made.
     """,
-
     'author': "My Company",
     'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Accounting',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base', 'contacts'],
-
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
-    #Aplications
     'application': True,
 }
